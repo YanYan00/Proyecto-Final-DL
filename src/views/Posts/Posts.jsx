da8f162ca135253ad4665e48adf641276040350d
Jean Rojas <jean.rojas@usach.cl>
2025-02-22 05:11:04 -0300

    Agregados los botones de eliminacion y edicion de publicaciones

--- src/views/Posts/Posts.jsx
+++ src/views/Posts/Posts.jsx
@@ -3,13 +3,16 @@ import './Posts.css';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { ItemsContext } from '../../context/ItemsContext';
-import { useNavigate } from 'react-router-dom';
 const Posts = () =>{
     const {id,token,obtenerPublicacionesBD,posts,agregarPublicacionBD,agregarProductoBD} = useContext(UserContext);
     const {consultarBD} = useContext(ItemsContext);
     const {categorias} = useContext(ItemsContext);
-    const navigate = useNavigate();
     const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [postsIds,setPostsIds] = useState({
+        publicacionId:null,
+        productoId
+    })
     const [formData,setFormData] = useState({
         nombre:'',
         stock:'',
@@ -37,24 +40,41 @@ const Posts = () =>{
                 alert("Todos los campos son requeridos");
                 return;
             }
-            const productoData ={
-                nombre:formData.nombre,
-                descripcion:formData.descripcion,
-                precio: parseInt(formData.precio),
-                stock: parseInt(formData.stock),
-                idCategoria: parseInt(formData.idCategoria)
-            };
-            const producto = await agregarProductoBD(productoData);
-
-            const publicacionData = {
-                titulo: formData.titulo,
-                descripcion:formData.descripcion,
-                precio:parseInt(formData.precio),
-                idUsuario:id,
-                idCategoria:parseInt(formData.idCategoria),
-                idProducto: producto.idproducto
+            if(isEditing){
+                await actualizarProductosBD(postsIds.productoId,{
+                    nombre: formData.nombre,
+                    descripcion: formData.descripcion,
+                    precio: parseInt(formData.precio),
+                    stock: parseInt(formData.stock),
+                    idCategoria: parseInt(formData.idCategoria)
+                });
+                await actualizarPublicacionBD(postsIds.publicacionId,{
+                    titulo: formData.titulo,
+                    descripcion: formData.descripcion,
+                    precio: parseInt(formData.precio),
+                    idCategoria: parseInt(formData.idCategoria)
+                })
             }
-            await agregarPublicacionBD(publicacionData)
+            else{
+                const productoData ={
+                    nombre:formData.nombre,
+                    descripcion:formData.descripcion,
+                    precio: parseInt(formData.precio),
+                    stock: parseInt(formData.stock),
+                    idCategoria: parseInt(formData.idCategoria)
+                };
+                const producto = await agregarProductoBD(productoData);
+
+                const publicacionData = {
+                    titulo: formData.titulo,
+                    descripcion:formData.descripcion,
+                    precio:parseInt(formData.precio),
+                    idUsuario:id,
+                    idCategoria:parseInt(formData.idCategoria),
+                    idProducto: producto.idproducto
+                }  
+                await agregarPublicacionBD(publicacionData) 
+            }
             setFormData({
                 nombre:'',
                 stock:'',
@@ -63,15 +83,59 @@ const Posts = () =>{
                 idCategoria:'',
                 titulo:''
             })
+            setIsEditing(false);
             setIsCreating(false);
+            setPostsIds({publicacionId:null,productoId:null});
             await obtenerPublicacionesBD(id);
             await consultarBD();
-            navigate('/');
         } catch (error) {
             console.error("Error al crear la publicacion",error);
-            alert("Hubo un error al crear la publicacion");
+            alert(`Hubo un error al ${isEditing ? 'actualizar' : 'crear'}  la publicacion`);
         }
     }
+    const handleEdit = (item) =>{
+        setPostsIds({
+            publicacionId: item.idpublicacion,
+            productoId: item.idproducto
+        })
+        setFormData({
+            nombre: item.nombre,
+            stock: item.stock,
+            titulo: item.titulo,
+            descripcion: item.descripcion,
+            precio: item.precio,
+            idCategoria: item.idcategoria
+        })
+        setIsEditing(true);
+    }
+    const handleDelete = async (publicacionId,productoId) => {
+        const confirmar = window.confirm('¿Estás seguro que quieres eliminar esta publicación')
+        if(confirmar){
+            try {
+                await eliminarPublicacion(publicacionId);
+                await eliminarProducto(productoId);
+                await obtenerPublicacionesBD(id);
+                await consultarBD();
+            } catch (error) {
+                alert('Error al eliminar la publicacion');
+            }
+        }
+    }
+    const renderPublications = () => (
+        <div className='cont-publicaciones'>
+            <button onClick={() => setIsCreating(true)} className="w-full mb-4 bg-blue-500 text-white py-2 rounded">Nueva publicación</button>
+            <div className="space-y-4">
+                {posts.map((item) => (
+                    <div key={item.idpublicacion} className="border p-4 rounded">
+                        <h4 className="font-bold">{item.titulo}</h4>
+                        <p className="text-gray-600">${item.precio.toLocaleString()}</p>
+                        <p>{item.descripcion}</p>
+                        <button onClick={()=> handleEdit(item)}>Editar</button>
+                        <button onClick={()=> handleDelete(item.idpublicacion,item.idproducto)}>X</button>
+                    </div>))}
+            </div>
+        </div>
+    )
     const renderForm = () => (
         <form onSubmit={handleSubmit}>
             <div className="mb-4">
@@ -159,14 +223,21 @@ const Posts = () =>{
                     type="submit"
                     className="bg-blue-500 text-white py-2 px-4 rounded"
                 >
-                    Crear Publicación
+                    {isEditing ? 'Guardar Cambios' : 'Crear Publicación'}
                 </button>
                 <button 
                     type="button"
-                    onClick={() => setIsCreating(false)}
-                    className="bg-gray-300 text-gray-700 py-2 px-4 rounded"
-                >
-                    Cancelar
+                    onClick={() => {setIsEditing(false); setIsCreating(false); 
+                        setFormData({
+                            nombre: '',
+                            stock: '',
+                            descripcion: '',
+                            precio: '',
+                            idCategoria: '',
+                            titulo: ''
+                        })
+                    }}
+                    className="bg-gray-300 text-gray-700 py-2 px-4 rounded">Cancelar
                 </button>
             </div>
         </form>
@@ -194,33 +265,10 @@ const Posts = () =>{
 
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
-            <h3 className="text-2xl font-bold mb-4">Mis publicaciones</h3>
-            
-            {!isCreating ? (
-                <>
-                    <button 
-                        onClick={() => setIsCreating(true)}
-                        className="w-full mb-4 bg-blue-500 text-white py-2 rounded"
-                    >
-                        Nueva publicación
-                    </button>
-
-                    <div className="space-y-4">
-                        {posts.map((item) => (
-                            <div 
-                                key={item.idpublicacion} 
-                                className="border p-4 rounded"
-                            >
-                                <h4 className="font-bold">{item.titulo}</h4>
-                                <p className="text-gray-600">${item.precio.toLocaleString()}</p>
-                                <p>{item.descripcion}</p>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            ) : (
-                renderForm()
-            )}
+            <h3 className="text-2xl font-bold mb-4">{isEditing ? 'Editar Publicacion' : 'Mis publicaciones'}</h3>
+            {(isCreating || isEditing) ?
+                renderDeleting() : renderPublications()
+            }
         </div>
     );
 };
