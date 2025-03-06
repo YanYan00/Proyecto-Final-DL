ecb3d1cd8c3ac11713c8f30b7645f15f4c4dcb99
Jean Rojas <jean.rojas@usach.cl>
2025-03-06 09:05:08 -0300

    Arreglando diseño a los pedidos

--- src/views/Orders/Orders.jsx
+++ src/views/Orders/Orders.jsx
@@ -36,26 +36,31 @@ const Orders = () =>{
                 </div>
             ):(
                 <div className='pedidos'>
-                    {pedidos.map((pedido) =>(
+                    {pedidos.map((pedido) => (
                         <div key={`${pedido.idpedido}-${pedido.idproducto}`} className='pedido'>
                             <div className='detalles-pedido'>
-                                {pedido.urlimagen &&(
+                                {pedido.urlimagen && (
                                     <div className='img-pedido'>
-                                        <img src={pedido.urlimagen} alt={pedido.nombre} />    
+                                        <img src={pedido.urlimagen} alt={pedido.nombre} />
                                     </div>
-                                    
                                 )}
                                 <div className='info-pedido'>
                                     <h4>{pedido.nombre}</h4>
                                     <span>{pedido.estado}</span>
-                                    <h5>Informacion del cliente</h5>
+                                    
+                                    <h5>Información del cliente</h5>
                                     <p><span className="font-medium">Nombre:</span> {pedido.nombrecomprador}</p>
-                                    <p><span className="font-medium">Dirección:</span> {pedido.direccioncomprador}</p>          
-                                    <span>Cantidad:{pedido.cantidad}</span>
-                                    <p>Precio unitario: ${pedido.precio}</p> 
-                                    <p><span>Precio total:</span> ${(pedido.precio * pedido.cantidad).toLocaleString()}</p>
-                                    <button onClick={() => confirmarEnvioBD(pedido.iddetalle, "Enviado")}>Enviar pedido</button>
-                                </div>     
+                                    <p><span className="font-medium">Dirección:</span> {pedido.direccioncomprador}</p>
+                                    
+                                    <h5>Detalles del pedido</h5>
+                                    <p><span className="font-medium">Cantidad:</span> {pedido.cantidad}</p>
+                                    <p><span className="font-medium">Precio unitario:</span> ${pedido.precio.toLocaleString()}</p>
+                                    <p><span className="font-medium">Precio total:</span> ${(pedido.precio * pedido.cantidad).toLocaleString()}</p>
+                                    
+                                    <button onClick={() => confirmarEnvioBD(pedido.iddetalle, "Enviado")}>
+                                        Enviar pedido
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     ))}
