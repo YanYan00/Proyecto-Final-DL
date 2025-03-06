ad030d13c8c913fea51d13c486dbebd1a155a2d2
Jean Rojas <jean.rojas@usach.cl>
2025-03-06 00:01:08 -0300

    Arreglando vista de home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,51 @@
+import CardItem from "../../components/CardItem/CardItem";
+import { useContext, useEffect, useState } from "react";
+import { ItemsContext } from "../../context/ItemsContext";
+import './Home.css'
+import { CartContext } from "../../context/CartContext";
+import { Form } from "react-bootstrap";
+
+const Home = () => {
+  const [itemsFiltrados, setItemsFiltrados] = useState([]);
+  const [botonActivo, setBotonActivo] = useState(null);
+  const [buscador, setBuscador] = useState('');
+  const [pagina, setPagina] = useState(1);
+  const { items, categorias } = useContext(ItemsContext);
+  const { añadirItem } = useContext(CartContext);
+  
+  const inicio = (pagina-1) * 6;
+  const final = inicio + 6;
+  
+  const actualItems = itemsFiltrados.length > 0 ? itemsFiltrados : items;
+  const mostrarItems = actualItems.slice(inicio, final);
+  
+useEffect(() => {
+  let filtradas = items;
+  if (botonActivo) {
+    filtradas = filtradas.filter(item => item.categoria === botonActivo.nombre);
+  }
+  if (buscador) {
+    filtradas = filtradas.filter(item => {
+      if (!item || (!item.nombre && !item.descripcion)) return false;
+      const nombre = item.nombre ? item.nombre.toLowerCase() : '';
+      const descripcion = item.descripcion ? item.descripcion.toLowerCase() : '';
+      const termino = buscador.toLowerCase();
+      return nombre.includes(termino) || descripcion.includes(termino);
+    });
+  }
+  setItemsFiltrados(filtradas);
+  setPagina(1);
+}, [botonActivo, buscador, items])
+  
+const filtrarItems = (categoria) => {
+  if (botonActivo?.nombre === categoria.nombre) {
+    setBotonActivo(null);
+    setBuscador('');
+    return;
+  }
+  setBotonActivo(categoria);
+  }
+}
 return (
   <div className="home-container">
     <div className="main-content">
